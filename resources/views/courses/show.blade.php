461672f1e3f18e2354aa57be02eb2a1a1655c27a
breezy11 <branimir.raguz@student.fsre.ba>
2020-09-22 12:25:43 +0200

    ...

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -2,4 +2,8 @@
 
 @section('content')
     <course-show :course="{{ $course }}" :user="{{ \Illuminate\Support\Facades\Auth::user() }}"></course-show>
+
+    <div>
+        @comments(['model' => $course])
+    </div>
 @endsection
